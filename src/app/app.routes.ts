d9a6c1095d35bf161060bc0cddcb2118e6dddfdf
kishoribihade <kishori.biade@gmail.com>
2024-07-31 21:28:51 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { FavoritePositionsComponent } from './Component/favorite-positions/favor
 import { JobDetailViewComponent } from './Component/job-detail-view/job-detail-view.component';
 
 export const routes: Routes = [
-    {path: "",component: JobBoardComponent},
+    {path: "", redirectTo: '/jobBoard', pathMatch:'full' },
     {path: "jobBoard", component: JobBoardComponent},
     {path: "favPos", component: FavoritePositionsComponent},
-    {path: "jobDetailView/:id", component: JobDetailViewComponent}
+    {path: "jobDetailView/:id", component: JobDetailViewComponent},
+    {path: "**", component: JobBoardComponent},
 ];
